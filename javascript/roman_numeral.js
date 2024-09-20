4729e4366296d3f1197f47a27e265bab93777183
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-09-20 10:30:13 -0400

    solution

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,5 +1,27 @@
 function romanNumeral(string) {
   // type your code here
+  //define romannumerals
+  romanDict = {
+    "I":1,
+    "V":5,
+    "X":10,
+    "L":50,
+    "C":100,
+    "D":500,
+    "M":1000
+  }
+  let total = 0;
+  for(let i = 0; i < string.length; i++){
+    let current = romanDict[string[i]]
+    let next = romanDict[string[i+1]]
+    if(current < next){
+      total -= current
+    } else{
+      total +=current
+    }
+  }
+  return total
+
 }
 
 if (require.main === module) {
